08d90da75c32984aeb41eea83f22738826b087d3
Skybeorn <Skybeorn@10.30.16.54>
2012-03-06 13:13:48 -0600

    Worked on the LineItem Class, Getting a Null Pointer Exception. Ran Debugger, it appears that the double isn't passing into the GetLineItemAsString()
    Moving onto other classes for now.

---
+++ src/discountstrategyme/LineItem.java
@@ -0,0 +1,75 @@
+package discountstrategyme;
+
+public class LineItem {
+
+    private double quantity;
+    private Product product;
+    private Product[] pDb;
+
+    public LineItem(String productId, double quantity) {
+        pDb = new Product[3];
+        Product milk = new Product("1", "Milk", 15.00);
+        milk.setDiscountStrategy(new XmasDiscount());
+        pDb[0] = milk;
+
+        Product juice = new Product("2", "Juice", 10.00);
+        juice.setDiscountStrategy(new QuantityDiscount());
+        pDb[1] = juice;
+
+        Product tea = new Product("3", "Tea", 5.00);
+        tea.setDiscountStrategy(new NoDiscount());
+        pDb[2] = milk;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    private Product findProduct(String productId) {
+        Product id = null;
+
+        for (Product p : pDb) {
+            if (productId.equals(p.getId())) {
+                id = p;
+                break;
+            }
+        }
+
+        return id;
+    }
+
+    public double getSubtotal() {
+        return (quantity * product.getUnitCost() - product.getCalculatedDiscountAmount(quantity));
+    }
+
+    public double getSubtotalDiscountAmount() {
+        return product.getCalculatedDiscountAmount(quantity);
+    }
+
+    public String getLineItemAsString() {
+        double discountAmt = product.getCalculatedDiscountAmount(quantity);
+        return product.getId() + "     " + product.getName()
+                + "     " + quantity + "     " + product.getUnitCost()
+                + "     " + getSubtotalDiscountAmount() + "    "
+                + getSubtotal();
+    }
+
+    public static void main(String[] args) {
+
+        LineItem p = new LineItem("2", 5.0);
+        String data = p.getLineItemAsString();
+        System.out.println(data);
+    }
+}
